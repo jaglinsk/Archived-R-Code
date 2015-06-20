4b1d34091d15c8a23b1377af3f00e98fbb5402df
John G <jglinski@ymail.com>
2015-06-20 10:03:57 -0400

    README

--- BoxOffice_All.R
+++ BoxOffice_All.R
@@ -1,26 +1,30 @@
 #The purpose of this code is to extract the top grossing movies of 2014 from the site BoxOfficeMojo.com
-library(rvest)
-library(plyr)
 
-#Load URL of interest
-boxofficeAll <- data.frame()
+boxofficeStart <- function(){
+  #Load libraries and create empty data.frame
+  library(rvest)
+  library(plyr)
 
+  boxofficeAll <<- data.frame()
+}
 
-for (i in 1990: 2014){
-  url <- html(paste("http://www.boxofficemojo.com/yearly/chart/?yr=",i,"&p=.htm/", sep=""))
+boxofficeScrape <- function(year){
+   
+  url <- html(paste("http://www.boxofficemojo.com/yearly/chart/?yr=",year,"&p=.htm/", sep=""))
   
-  #Look for all html elements that contain relevant box office data
+  #Look for all html elements that contain relevant box office data for given year
   k<- url %>%
     html_nodes("td") %>%
     html_text()
-  if (i > 2002){
-    #Since there was no unique html code to identify, I manually identified what "td" elements I wanted in my dataset
+	
+  if (year >= 2002){
+    #Capture only <td> elements that contain data of interest 
     k<- k[c(13:14, 17:920)]
     
-    #Added one missing value that was not captured
+    #Add missing value excluded from previous line of code
     k<- append(k, "Studio", after = 2)
     
-    #Split two values in order to properly assign headers in the future data.frame
+    #Split two values in order to properly align headers with columns
     x<- unlist(strsplit(k[4], "/"))
     k <- k[-4]
     k<- append(k, x, after = 3)
@@ -47,13 +51,13 @@ for (i in 1990: 2014){
     
     x <- as.POSIXlt(k[,8])
     y <- as.POSIXlt(k[,9])
-    x$year <- (i-1900)
+    x$year <- (year-1900)
     
-    for(j in 1:length(y)){
-      if((y$yday[j] > x$yday[j]) & (!is.na(y$yday[j]))){
+    #for(j in 1:length(y)){
+      #if((y$yday[j] > x$yday[j]) & (!is.na(y$yday[j]))){
         
-        y$year[j] <- y$year[j] - 1}
-    }
+        #y$year[j] <- y$year[j] - 1}
+    #}
     
     k[,8] <- x$year
     k[,8] <- as.Date(as.POSIXct(x))
@@ -61,19 +65,19 @@ for (i in 1990: 2014){
     k[,9] <- y$year
     k[,9] <- as.Date(as.POSIXct(y))
     
-    boxofficeAll <- rbind.fill(boxofficeAll, k)
+    boxofficeAll <<- rbind.fill(boxofficeAll, k)
     rm(k)
     
-    Sys.sleep(5)
+    
   }
   else {
-    #Since there was no unique html code to identify, I manually identified what "td" elements I wanted in my dataset
+    #Capture only <td> elements that contain data of interest 
     k<- k[c(13:14, 17:819)]
     
-    #Added one missing value that was not captured
+    #Add missing value excluded from previous line of code
     k<- append(k, "Studio", after = 2)
     
-    #Split two values in order to properly assign headers in the future data.frame
+    #Split two values in order to properly align headers with columns
     x<- unlist(strsplit(k[4], "/"))
     k <- k[-4]
     k<- append(k, x, after = 3)
@@ -100,21 +104,24 @@ for (i in 1990: 2014){
     
     x <- as.POSIXlt(k[,8])
     
-    x$year <- (i-1900)
+    x$year <- (year-1900)
     
     
     k[,8] <- x$year
     k[,8] <- as.Date(as.POSIXct(x))
     
     
-    boxofficeAll <- rbind.fill(boxofficeAll, k)
+    boxofficeAll <<- rbind.fill(boxofficeAll, k)
     rm(k)
-    Sys.sleep(sample(10:20, 1))
+    
   }
-  
 }
+  
 
-summary(boxofficeAll)
-hist(na.omit(boxofficeall[,4]))
-myts <- ts(boxofficeAll, frequency = 12)
-
+boxofficeSTATS <- function (){
+  summary(boxofficeAll)
+  hist(na.omit(boxofficeAll[,4]))
+  myts <- ts(boxofficeAll, frequency = 12)
+  plot(myts)
+}
+#NEED TO FIX CLOSE YEAR AND TIME SERIES
--- README.md
+++ README.md
@@ -4,4 +4,4 @@ This space was to share what I've created as I continue to expand on my coding s
 
 BoxOffice_All.R: Webscraping project done to learn the finer points of the R language using movie box office data. 
 
-MeanSample. R: Some sample statistical sampling code based on resources
+MeanSample. R: Some sample statistical sampling code based on resources found online
